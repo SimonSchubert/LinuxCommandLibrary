10a862570ce1449c948a73fdca10549d8193c52f
Simon Schubert <simon@librem.one>
2016-01-24 11:58:59 +0100

    added missing files

---
+++ app/src/main/java/com/inspiredandroid/linuxcommandbibliotheca/models/Command.java
@@ -0,0 +1,53 @@
+package com.inspiredandroid.linuxcommandbibliotheca.models;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by simon on 24.01.16.
+ */
+public class Command extends RealmObject{
+    private int id;
+    private int category;
+    private String name;
+    private String description;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public int getCategory()
+    {
+        return category;
+    }
+
+    public void setCategory(int category)
+    {
+        this.category = category;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+}
---
+++ app/src/main/java/com/inspiredandroid/linuxcommandbibliotheca/models/Quiz.java
@@ -0,0 +1,76 @@
+package com.inspiredandroid.linuxcommandbibliotheca.models;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by simon on 24.01.16.
+ */
+public class Quiz extends RealmObject{
+
+    private int id;
+    private String name;
+    private int difficulty;
+    private int type;
+    private String description;
+    private String extra;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int _id)
+    {
+        this.id = _id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getDifficulty()
+    {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty)
+    {
+        this.difficulty = difficulty;
+    }
+
+    public int getType()
+    {
+        return type;
+    }
+
+    public void setType(int type)
+    {
+        this.type = type;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public String getExtra()
+    {
+        return extra;
+    }
+
+    public void setExtra(String extra)
+    {
+        this.extra = extra;
+    }
+}
