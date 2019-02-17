53023fcdf91e19f8a34e108ba5e257f512fcf663
Simon Schubert <simon@librem.one>
2019-02-17 13:31:03 +0100

    Convert to gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,94 @@
+buildscript {
+    repositories {
+        jcenter()
+        mavenCentral()
+        google()
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("io.realm:realm-gradle-plugin:5.9.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
+    }
+}
+
+apply {
+    plugin("realm-android")
+}
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    kotlin("android.extensions")
+    id("kotlin-kapt")
+}
+
+repositories {
+    maven("https://maven.google.com")
+}
+
+android {
+    compileSdkVersion(28)
+
+    defaultConfig {
+        applicationId = "com.inspiredandroid.linuxcommandbibliotheca"
+        minSdkVersion(17)
+        targetSdkVersion(28)
+        versionCode = 64
+        versionName = "2.0.2"
+
+        resConfigs(listOf("en"))
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables.useSupportLibrary = true
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+        getByName("release") {
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    lintOptions {
+        isWarningsAsErrors = true
+        isAbortOnError = true
+    }
+
+    compileOptions {
+        setSourceCompatibility(JavaVersion.VERSION_1_8)
+        setTargetCompatibility(JavaVersion.VERSION_1_8)
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.anko:anko:0.10.8") {
+        exclude(module = "kotlin-stdlib")
+    }
+    implementation("com.android.support.constraint:constraint-layout:1.1.3")
+    implementation("com.android.support:support-annotations:28.0.0")
+    implementation("com.android.support:appcompat-v7:28.0.0")
+    implementation("com.android.support:design:28.0.0")
+    implementation("com.android.support:recyclerview-v7:28.0.0")
+    implementation("com.android.support:support-v4:28.0.0")
+    implementation("com.android.support:support-vector-drawable:28.0.0")
+    implementation("io.realm:android-adapters:3.1.0")
+    implementation("com.google.firebase:firebase-core:16.0.7")
+    implementation("com.google.firebase:firebase-appindexing:17.1.0")
+    implementation("com.google.firebase:firebase-crash:16.2.1")
+    implementation("com.futuremind.recyclerfastscroll:fastscroll:0.2.5")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("com.android.support.test:runner:1.0.2")
+    androidTestImplementation("com.android.support.test:rules:1.0.2")
+    androidTestImplementation("com.android.support:support-annotations:28.0.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.21")
+}
+
+apply {
+    plugin("com.google.gms.google-services")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+    id("com.github.ben-manes.versions") version "0.20.0"
+}
+
+buildscript {
+    repositories {
+        jcenter()
+        google()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
+        classpath("com.google.gms:google-services:4.2.0")
+    }
+}
+
+allprojects {
+    repositories {
+        mavenLocal()
+        google()
+        jcenter()
+        maven("https://jitpack.io")
+    }
+}
+
+tasks.register("clean", Delete::class.java) {
+    delete(rootProject.buildDir)
+}
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
+    resolutionStrategy {
+        componentSelection {
+            all {
+                val rejected = listOf("alpha", "beta", "rc", "cr", "m", "preview")
+                        .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-]*") }
+                        .any { it.matches(candidate.version) }
+                if (rejected) {
+                    reject("Release candidate")
+                }
+            }
+        }
+    }
+    // optional parameters
+    checkForGradleUpdate = true
+}
