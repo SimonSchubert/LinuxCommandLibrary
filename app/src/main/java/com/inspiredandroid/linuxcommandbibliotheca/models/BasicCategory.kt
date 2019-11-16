365a9da701a0ab28c4fcd56234ffd4062d48f21c
Simon Schubert <sschubert89@gmail.com>
2019-11-16 12:17:51 +0100

    Add permission basics and tips

--- app/src/main/java/com/inspiredandroid/linuxcommandbibliotheca/models/BasicCategory.kt
+++ app/src/main/java/com/inspiredandroid/linuxcommandbibliotheca/models/BasicCategory.kt
@@ -29,21 +29,23 @@ open class BasicCategory : RealmObject() {
 
     val iconResourceId: Int
         get() {
-            when (id) {
-                247 -> return R.drawable.ic_user_male_circle_40dp
-                37 -> return R.drawable.ic_folder_black_40dp
-                246 -> return R.drawable.ic_info_40dp
-                21 -> return R.drawable.ic_settings_black_40dp
-                43 -> return R.drawable.ic_video_trimming_40dp
-                243 -> return R.drawable.ic_network_card_40dp
-                179 -> return R.drawable.ic_search_40dp
-                248 -> return R.drawable.ic_skull_40dp
-                249 -> return R.drawable.ic_package_40
-                29 -> return R.drawable.ic_videogame_asset_black_40dp
-                250 -> return R.drawable.ic_icons8_hand_with_pen
-                251 -> return R.drawable.ic_vpn_key_black_24dp
-                252 -> return R.drawable.ic_icons8_github
+            return when (id) {
+                247 -> R.drawable.ic_user_male_circle_40dp
+                37 -> R.drawable.ic_folder_black_40dp
+                246 -> R.drawable.ic_info_40dp
+                21 -> R.drawable.ic_settings_black_40dp
+                43 -> R.drawable.ic_video_trimming_40dp
+                243 -> R.drawable.ic_network_card_40dp
+                179 -> R.drawable.ic_search_40dp
+                248 -> R.drawable.ic_skull_40dp
+                249 -> R.drawable.ic_package_40
+                29 -> R.drawable.ic_videogame_asset_black_40dp
+                250 -> R.drawable.ic_icons8_hand_with_pen
+                251 -> R.drawable.ic_vpn_key_black_24dp
+                252 -> R.drawable.ic_icons8_github
+                209 -> R.drawable.ic_icons8_print
+                else -> R.drawable.ic_user_male_circle_40dp
             }
-            return R.drawable.ic_user_male_circle_40dp
+
         }
 }
