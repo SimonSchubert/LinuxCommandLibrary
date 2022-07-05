ca4de427b4837ed0ba84e013921cb1b005468e48
SimonSchubert <simon@librem.one>
2022-07-05 11:55:27 +0200

    Add MarkdownBuilder to automatically generate markdown

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -1,12 +1,8 @@
-import org.example.common.App
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.linuxcommandlibrary.CommandDatabase
-import com.squareup.sqldelight.db.SqlDriver
-import org.example.common.DatabaseHelper
-import org.example.common.databaseHelper
 import org.example.common.initDatabase
 
 fun main() = application {
