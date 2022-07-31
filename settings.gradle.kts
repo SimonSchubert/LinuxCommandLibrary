7ab22b560581c1ccec03a69c5c6fc26c5d23c597
SimonSchubert <simon@librem.one>
2022-07-31 22:22:57 +0200

    Upgrade gradle, kotlinx-html

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,10 @@ pluginManagement {
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-    
+
 }
 rootProject.name = "Linux Command Library"
 
 
-include(":android")
-include(":desktop")
-include(":common")
+include(":android", ":desktop", ":common")
 
