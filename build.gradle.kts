66d1df7b344762ee6881c81b3a3fcca6716e19a0
SimonSchubert <simon@librem.one>
2022-08-11 11:52:36 +0200

    Upgrade kotlin and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
-        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("com.android.tools.build:gradle:7.2.2")
         classpath( "com.squareup.sqldelight:gradle-plugin:1.5.3")
     }
 }
