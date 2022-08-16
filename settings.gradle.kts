9586c5bf2bf64ac45db65965a7c6b82b10ebd2a1
SimonSchubert <simon@librem.one>
2022-08-16 11:19:23 +0200

    Remove updated compose maven repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,9 @@ pluginManagement {
         google()
         gradlePluginPortal()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 rootProject.name = "Linux Command Library"
 
 
 include(":android", ":desktop", ":common", ":cli")
-
