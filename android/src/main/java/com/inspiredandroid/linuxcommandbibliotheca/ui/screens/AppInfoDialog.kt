78324199c357116b4601b3882fffa09a86f78a00
SimonSchubert <simon@librem.one>
2022-11-26 13:22:24 +0100

    Add bookmark feedback dialog

--- android/src/main/java/com/inspiredandroid/linuxcommandbibliotheca/ui/screens/AppInfoDialog.kt
+++ android/src/main/java/com/inspiredandroid/linuxcommandbibliotheca/ui/screens/AppInfoDialog.kt
@@ -1,29 +1,39 @@
 package com.inspiredandroid.linuxcommandbibliotheca.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.inspiredandroid.linuxcommandbibliotheca.R
+import com.inspiredandroid.linuxcommandbibliotheca.ui.composables.SectionTitle
 import com.inspiredandroid.linuxcommandbibliotheca.ui.theme.LinuxTheme
+import kotlinx.coroutines.delay
 
 /* Copyright 2022 Simon Schubert
  *
@@ -57,9 +67,20 @@ fun AppInfoDialog(showDialog: MutableState<Boolean>) {
                         style = MaterialTheme.typography.h5,
                         modifier = Modifier.padding(8.dp)
                     )
-                    Button(content = { Text("Rate the app") }, onClick = {
-                        uriHandler.openUri("https://play.google.com/store/apps/details?id=com.inspiredandroid.linuxcommandbibliotheca")
-                    }, modifier = Modifier.padding(start = 6.dp))
+                    Row {
+                        Button(content = { Text("Rate the app") }, onClick = {
+                            uriHandler.openUri("https://play.google.com/store/apps/details?id=com.inspiredandroid.linuxcommandbibliotheca")
+                        }, modifier = Modifier.padding(start = 6.dp))
+                        IconButton(onClick = {
+                            uriHandler.openUri("https://github.com/SimonSchubert/LinuxCommandLibrary")
+                        }) {
+                            Icon(
+                                painterResource(R.drawable.ic_icons8_github),
+                                contentDescription = null,
+                                modifier = Modifier.size(40.dp)
+                            )
+                        }
+                    }
                     Column(
                         modifier = Modifier
                             .padding(8.dp)
