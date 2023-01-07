5c4d96d344611e359c8868b3934b93df21baa61f
SimonSchubert <simon@librem.one>
2023-01-07 12:07:56 +0100

    Upgrade sdks

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,15 +15,15 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.6.1")
     implementation("androidx.compose.material:material:1.3.1")
     implementation("androidx.navigation:navigation-compose:2.5.3")
-    implementation("com.google.accompanist:accompanist-appcompat-theme:0.27.1")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.1")
+    implementation("com.google.accompanist:accompanist-appcompat-theme:0.28.0")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
     implementation("androidx.preference:preference:1.2.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.3.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.2")
 
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.3.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.3.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.3.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.3.2")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.3.2")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.3.2")
 }
 
 android {
