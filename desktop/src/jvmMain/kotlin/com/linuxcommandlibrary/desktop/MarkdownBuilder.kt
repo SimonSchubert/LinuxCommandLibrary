3b2e7299385f3fad1d0232e9c5d2d20622224c69
SimonSchubert <simon@librem.one>
2023-01-07 12:11:21 +0100

    Add commands and update meta data

--- desktop/src/jvmMain/kotlin/com/linuxcommandlibrary/desktop/MarkdownBuilder.kt
+++ desktop/src/jvmMain/kotlin/com/linuxcommandlibrary/desktop/MarkdownBuilder.kt
@@ -59,7 +59,7 @@ class MarkdownBuilder {
         stream.appendLine()
         stream.appendLine("<img src=\"https://raw.githubusercontent.com/SimonSchubert/LinuxCommandBibliotheca/master/art/screen-cli-1.png\" width=\"300\">")
         stream.appendLine()
-        stream.appendLine("Execute `[gradle](https://docs.gradle.org/current/userguide/command_line_interface.html#sec:command_line_executing_tasks) :cli:buildFatJar2` to create jar file for Linux, Windows and Mac.")
+        stream.appendLine("Execute `gradle :cli:buildFatJar2` to create jar file for Linux, Windows and Mac.")
         stream.appendLine()
         stream.appendLine("### Categories")
         stream.appendLine()
