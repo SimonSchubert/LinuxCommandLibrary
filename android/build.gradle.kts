c41426585925b16acf88e3389f600dbe3321de34
Simon Schubert <sschubert@gala.games>
2024-12-30 21:54:02 +0100

    Upgrade version code

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         applicationId = "com.inspiredandroid.linuxcommandbibliotheca"
         minSdk = 24
         targetSdk = 35
-        versionCode = 91
+        versionCode = 92
         versionName = project.version.toString()
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
