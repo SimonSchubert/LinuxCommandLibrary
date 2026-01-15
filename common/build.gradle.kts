1ff99bc0b7ed28212b5347a0cc9701fbdbda0ef3
Simon Schubert <simon@librem.one>
2026-01-15 12:57:49 +0100

    Release

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -44,7 +44,7 @@ kotlin {
 }
 
 android {
-    compileSdk = 35
+    compileSdk = 36
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     defaultConfig {
         minSdk = 24
