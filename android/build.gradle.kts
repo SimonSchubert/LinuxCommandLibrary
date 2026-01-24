b79d1a8f1a5c47f8c1110186ed98882b4886c432
Simon Schubert <simon@librem.one>
2026-01-24 16:58:33 +0100

    Release

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -50,6 +50,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
         getByName("debug") {
             isMinifyEnabled = false
