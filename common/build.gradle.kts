cd7bed9c67004316e16eef7fc662e1608c67d349
Simon Schubert <simon@librem.one>
2026-05-15 18:12:46 +0200

    Upgrade dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -30,7 +30,6 @@ kotlin {
     }
 
     listOf(
-        iosX64(),
         iosArm64(),
         iosSimulatorArm64(),
     ).forEach {
--- viewmodels/build.gradle.kts
+++ viewmodels/build.gradle.kts
@@ -32,7 +32,6 @@ kotlin {
     }
 
     listOf(
-        iosX64(),
         iosArm64(),
         iosSimulatorArm64(),
     ).forEach {
